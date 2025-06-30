4875243c0bbc6ae2beeaf71d8d6ae3009cf15766
techservices-dialogedu-com <101670733+techservices-dialogedu-com@users.noreply.github.com>
2025-06-30 14:50:04 -0400

    Update app.py
    new

--- app.py
+++ app.py
@@ -1,80 +1,77 @@
 from flask import Flask, request, make_response
-from signxml import XMLSigner, methods
+from signxml import XMLSigner
 from lxml import etree
 import base64
 import datetime
 
 app = Flask(__name__)
 
-# Your SSO URLs
-ISSUER      = "https://sso.dialogedu.com"
+# SSO configuration constants
+ISSUER = "https://sso.dialogedu.com"
 DESTINATION = "https://accounts.zohoportal.com/accounts/csamlresponse/10098904568"
 
 @app.route("/login")
 def login():
-    # Extract required query params
-    email        = request.args.get("email")
-    first_name   = request.args.get("firstName")
-    last_name    = request.args.get("lastName")
-    relay_state  = request.args.get("redirect")
+    # Extract query parameters
+    email       = request.args.get("email")
+    first_name  = request.args.get("firstName")
+    last_name   = request.args.get("lastName")
+    relay_state = request.args.get("redirect")
 
+    # Validate inputs
     if not all([email, first_name, last_name, relay_state]):
-        return "Missing one or more required parameters: email, firstName, lastName, redirect", 400
+        return "Missing required parameters", 400
 
-    # Timestamps & IDs
-    now          = datetime.datetime.utcnow()
+    # Timestamps and response ID
+    now = datetime.datetime.utcnow()
     issue_instant = now.strftime("%Y-%m-%dT%H:%M:%SZ")
-    response_id  = "_{}".format(int(now.timestamp()))
+    response_id = f"_response_{int(now.timestamp())}"
 
     # Build raw SAML Response XML
-    xml_template = f"""
+    saml_xml = f"""
     <samlp:Response xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
-                    ID="{response_id}" Version="2.0" IssueInstant="{issue_instant}"
-                    Destination="{DESTINATION}">
+                    ID="{response_id}" Version="2.0" IssueInstant="{issue_instant}" Destination="{DESTINATION}">
       <saml:Issuer xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion">{ISSUER}</saml:Issuer>
-      <saml:Assertion xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
-                      ID="assertion_{response_id}" IssueInstant="{issue_instant}" Version="2.0">
+      <saml:Assertion xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" IssueInstant="{issue_instant}" Version="2.0">
         <saml:Issuer>{ISSUER}</saml:Issuer>
         <saml:Subject>
           <saml:NameID>{email}</saml:NameID>
         </saml:Subject>
         <saml:AttributeStatement>
-          <saml:Attribute Name="Email">
-            <saml:AttributeValue>{email}</saml:AttributeValue>
-          </saml:Attribute>
-          <saml:Attribute Name="FirstName">
-            <saml:AttributeValue>{first_name}</saml:AttributeValue>
-          </saml:Attribute>
-          <saml:Attribute Name="LastName">
-            <saml:AttributeValue>{last_name}</saml:AttributeValue>
-          </saml:Attribute>
+          <saml:Attribute Name="Email"><saml:AttributeValue>{email}</saml:AttributeValue></saml:Attribute>
+          <saml:Attribute Name="FirstName"><saml:AttributeValue>{first_name}</saml:AttributeValue></saml:Attribute>
+          <saml:Attribute Name="LastName"><saml:AttributeValue>{last_name}</saml:AttributeValue></saml:Attribute>
         </saml:AttributeStatement>
       </saml:Assertion>
     </samlp:Response>
     """
 
-    # Parse & Sign
-    xml_tree = etree.fromstring(xml_template.encode("utf-8"))
-    key_data  = open("key.pem", "rb").read()
-    cert_data = open("cert.pem", "rb").read()
+    # Parse and sign the XML
+    xml_tree = etree.fromstring(saml_xml.encode("utf-8"))
 
+    # Read key and certificate
+    with open("key.pem", "rb") as key_file:
+        key_data = key_file.read()
+    with open("cert.pem", "rb") as cert_file:
+        cert_data = cert_file.read()
+
+    # Sign the XML (omit method parameter to use default enveloped)
     signer = XMLSigner(
-        method=methods.enveloped,             # exactly the enum member your REPL listed
         signature_algorithm="rsa-sha256",
         digest_algorithm="sha256"
     )
     signed_xml = signer.sign(xml_tree, key=key_data, cert=cert_data)
 
-    # Base64-encode and render auto-POST form
+    # Base64 encode and build auto-post form
     b64_response = base64.b64encode(etree.tostring(signed_xml)).decode("utf-8")
     html = f"""
     <html>
-    <body onload="document.forms[0].submit()">
-      <form method="post" action="{DESTINATION}">
-        <input type="hidden" name="SAMLResponse" value="{b64_response}" />
-        <input type="hidden" name="RelayState"    value="{relay_state}" />
-      </form>
-    </body>
+      <body onload="document.forms[0].submit()">
+        <form method="post" action="{DESTINATION}">
+          <input type="hidden" name="SAMLResponse" value="{b64_response}"/>
+          <input type="hidden" name="RelayState" value="{relay_state}"/>
+        </form>
+      </body>
     </html>
     """
 
